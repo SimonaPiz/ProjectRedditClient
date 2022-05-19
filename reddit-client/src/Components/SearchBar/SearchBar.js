37cac87a0af8e3099bc25f8490cb4d731a4b9462
Simona Pizio <simona.pizio@gmail.com>
2022-05-19 15:52:18 +0200

    Add SearchBar Component and connect it to NavBar

---
+++ reddit-client/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import './SearchBar.css';
+
+export default function SearchBar() {
+  return (
+    <div id='searchBar'>            
+      <form action="/search/" id='searchContent' autocomplete="off" method='get' role="search">
+        <div><img src='./iconSearch.svg'/></div>
+        <input type="search"  name="searchTerm" placeholder="Cerca su Reddit" value="" size='40'/>
+        <button className='searchButton'>Search</button>
+      </form>      
+    </div>
+  );
+}
