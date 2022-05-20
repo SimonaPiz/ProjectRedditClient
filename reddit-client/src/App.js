fc4b297f3335e801bdd873d205c7e69bc2fb9168
Simona Pizio <simona.pizio@gmail.com>
2022-05-20 11:05:57 +0200

    Create SectionSubreddits component

--- reddit-client/src/App.js
+++ reddit-client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import SectionSubreddits from './Components/Subreddit/SectionSubreddits';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      //subreddits
+      <SectionSubreddits/>
       //posts
     </div>
   );
