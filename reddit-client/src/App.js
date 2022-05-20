307c976c63b4b8c7bcc8e4264d3051e1aac7ceb4
Simona Pizio <simona.pizio@gmail.com>
2022-05-20 11:21:02 +0200

    Create SectionPosts component

--- reddit-client/src/App.js
+++ reddit-client/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import SectionPosts from './Components/SectionPosts/SectionPosts';
 import SectionSubreddits from './Components/Subreddit/SectionSubreddits';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <SectionSubreddits/>
-      //posts
+      <main>
+        <SectionSubreddits/>
+        <SectionPosts/>
+      </main>
     </div>
   );
 }
---
+++ reddit-client/src/Components/SectionPosts/SectionPosts.js
@@ -0,0 +1,10 @@
+import './SectionPosts.css';
+
+export default function SectionPosts() {
+  return (
+    <section className='posts'>
+      <h2>Posts</h2>
+      
+    </section>
+  );
+}
