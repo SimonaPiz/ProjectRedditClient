18a11af558f31028c0db129fe5c30d07c6af543a
Simona Pizio <simona.pizio@gmail.com>
2022-05-31 16:40:02 +0200

    Add new page HomePage includes sectionSubreddits and sectionPosts

---
+++ reddit-client/src/Components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import './Homepage.css';
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <main>
+      <SectionSubreddits/>
+      <SectionPosts/>
+    </main>
+  );
+};
