7b76afe687d6c296eb98fe029601614ffaeb6fbf
Simona Pizio <simona.pizio@gmail.com>
2022-06-07 09:50:41 +0200

    Update SectionPosts for when load single subreddit posts

--- reddit-client/src/features/SectionPosts/SectionPosts.js
+++ reddit-client/src/features/SectionPosts/SectionPosts.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import './SectionPosts.css';
 import Post from '../../Components/Post/Post';
@@ -9,23 +10,32 @@ export default function SectionPosts() {
   const posts = useSelector((state) => state.sectionPosts.posts);
   const subreddits = useSelector((state) => state.sectionPosts.subredditPosts);
   const subIsLoad = useSelector((state) => state.sectionPosts.subIsLoad);
+  const subObj = useSelector((state) => state.detailSubreddit.subreddit);
   const dispatch = useDispatch();
+  let { subreddit } = useParams();
+  console.log(subreddit);
   
   useEffect(() => {
-    dispatch(fetchPosts('/r/popular'));     
-  }, [dispatch]);
+    if(subreddit) {
+      dispatch(fetchPosts('/r/' + subreddit));
+    } else {
+      dispatch(fetchPosts('/r/popular'));
+    }    
+  }, [dispatch, subreddit]);
 
   useEffect(() => {
-    for(let i=0; i<posts.length; i++) {
-      let subName = posts[i].subreddit;
-      dispatch(fetchSubredditByName(subName));
-    };
-  }, [posts]);
+    if(!subreddit){
+      for(let i=0; i<posts.length; i++) {
+        let subName = posts[i].subreddit;
+        dispatch(fetchSubredditByName(subName));
+      };
+    }
+  }, [posts, subreddit, dispatch]);
   
   if(posts === undefined) {
     return (
       <section className='posts'>
-        <h2>Popular Posts</h2>
+        <h2>Posts</h2>
         <p><br/>Loading posts...</p>
       </section>
     );
@@ -44,13 +54,13 @@ export default function SectionPosts() {
 
   return (
     <section className='posts'>
-      <h2>Popular Posts</h2>
+      <h2>{!subreddit ? 'Popular Posts' : 'Posts'}</h2>
       {posts.length > 0 ? posts.map((post) => {
         return(
         <Post
           subreddit={subIsLoad ? 
             findSub(post.subreddit)
-            : ''}
+            : subObj}
           post={post}
           key={post.id}
           id={post.id}
